aa6282d6b579b873339534b254016cb223b1b0ba
Xuefeng education <40351998+xuefengedu@users.noreply.github.com>
2018-12-20 21:54:16 +0800

    Update neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -132,7 +132,7 @@ namespace ColorBit {
             for (let i = 0; i < 16; ++i) {
                 const index=16-i;
                 const a=pixeloffset1 >> index;
-                if((a & 1)==1) 
+                if((a & 1)==0) 
                     this.setPixelRGB(i >> 0, 0);
                 else 
                     this.setPixelRGB(i >> 0, rgb >> 0);
@@ -140,7 +140,7 @@ namespace ColorBit {
             for (let i = 16; i < 25; ++i) {
                 const index=32-i;
                 const a=pixeloffset2 >> index;
-                if((a & 1)==1)
+                if((a & 1)==0)
                     this.setPixelRGB(i >> 0, 0);
                 else 
                     this.setPixelRGB(i >> 0, rgb >> 0);
